e50ad1901debee80984a56cc25f4edf6652a6867
Ethan Chiu <ethanchiu940520@gmail.com>
2025-07-24 12:10:41 +0800

    Update: main

--- calendar_ai_agent_web_app/frontend/src/index.js
+++ calendar_ai_agent_web_app/frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import EventForm from './EventForm';
+import './tailwind.output.css'; // 👈 MUST be imported here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
