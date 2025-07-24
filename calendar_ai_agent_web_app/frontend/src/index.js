d27eb3dbbbe8426ec1394445b415ddd5e86c1000
Ethan Chiu <ethanchiu940520@gmail.com>
2025-07-24 17:50:58 +0800

    Add: Event confirmation prompt section

--- calendar_ai_agent_web_app/frontend/src/index.js
+++ calendar_ai_agent_web_app/frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import EventForm from './EventForm';
+import EmailConfirmationPrompt from './EmailConfirmationPrompt'
 import './tailwind.output.css'; // 👈 MUST be imported here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <EventForm />
+    <EmailConfirmationPrompt />
   </React.StrictMode>
 );
