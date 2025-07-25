e340c36b21dcfa9b3661bc0294c31a1a076b019e
Ethan Chiu <ethanchiu940520@gmail.com>
2025-07-25 17:18:16 +0800

    Add: model and client for llama3.1:8b

--- calendar_ai_agent_web_app/backend/config.py
+++ calendar_ai_agent_web_app/backend/config.py
@@ -2,9 +2,12 @@
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
+from ollama import Client
 
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 model = "gpt-4o"
 
+client_llama = Client(host='http://192.168.50.214:11435')
+model_llama = 'llama3.1:8b'
