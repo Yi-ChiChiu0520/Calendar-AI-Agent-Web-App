8a128dbb11fb915c674798999fb5c3d874423c05
Ethan Chiu <ethanchiu940520@gmail.com>
2025-07-28 13:15:31 +0800

    Add: new fine tuned model specifically for extracting list event

--- calendar_ai_agent_web_app/backend/config.py
+++ calendar_ai_agent_web_app/backend/config.py
@@ -11,3 +11,5 @@ model = "gpt-4o"
 
 client_llama = Client(host='http://192.168.50.214:11435')
 model_llama = 'llama3.1:8b'
+
+model_fine_tuned = "ft:gpt-4.1-mini-2025-04-14:personal:list-calendar-model:By8kn4z8"
